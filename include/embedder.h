6f46fe3bdfaf47f741f5cd3e38509670e0cef53b
YwaGenemy <a.a.kolodeznikov@gmail.com>
2026-06-05 20:29:34 +0300

    feat: add embedder interface.

---
+++ include/embedder.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#include "index.h"
+#include "index_stats.h"
+#include "ad.h"
+
+#include <bits/stdc++.h> // ВРЕМЕННО!!!
+
+class Embedder{
+private:
+    size_t dimension_ = 0;
+public:
+    explicit Embedder(size_t dimension = 128);
+        
+    std::vector<float> Embed(const std::string& text) const;
+    size_t Dimension()const;
+};
