28b33fbe7dfe7b9843e66c4330b233fc31d6b213
YwaGenemy <a.a.kolodeznikov@gmail.com>
2026-06-12 20:49:11 +0300

    feat(cli): add basic cli entrypoint.

---
+++ include/cli.h
@@ -0,0 +1,6 @@
+#pragma once
+
+class Cli {
+public:
+    void Run();
+};
---
+++ src/cli.cpp
@@ -0,0 +1,8 @@
+#include "../include/cli.h"
+
+#include <iostream>
+
+void Cli::Run() {
+    std::cout << "Search Engine\n";
+    std::cout << "Type 'help' to see available commands.\n";
+}
